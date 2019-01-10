83cb3c024e261de3e08f33ab6f80a86d361aca7c
harrisonCassar - Laptop <harrison.cassar@gmail.com>
2019-01-09 17:07:26 -0800

    Added P1 Visual Studio Project with some dummy code in main for testing

---
+++ P1/P1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	int b = 3;
+
+	cout << "Hi!" << endl;
+
+	if (true)
+	{
+		int c[10];
+
+		cout << "Wow!" << endl;
+	}
+
+	cout << "Nice!" << endl;
+}
