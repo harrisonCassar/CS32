4d2af4d457cf95751a634ae157209157ab327f37
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2019-01-28 17:25:47 -0800

    Began adding more test cases for last part of report

--- P2/extraTestSet.cpp
+++ P2/extraTestSet.cpp
@@ -6,22 +6,47 @@ using namespace std;
 
 void test()
 {
-    Set ss;
-    cout << "1" << endl;
-    assert(ss.insert("roti"));
-    cout << "2" << endl;
-    assert(ss.insert("pita"));
-    cout << "3" << endl;
-    assert(ss.size() == 2);
-    cout << "4" << endl;
-    assert(ss.contains("pita"));
-    cout << "5" << endl;
+    Set s1;
+
+    assert(s1.insert("roti"));
+    assert(s1.insert("pita"));
+    assert(s1.size() == 2);
+    assert(s1.contains("pita"));
+
     ItemType x = "laobing";
-    cout << "6" << endl;
-    assert(ss.get(0, x) && x == "pita");
-    cout << "7" << endl;
-    assert(ss.get(1, x) && x == "roti");
-    cout << "8" << endl;
+    assert(s1.get(0, x) && x == "pita");
+    assert(s1.get(1, x) && x == "roti");
+
+    //default constructor
+    Set ss;
+
+    //for an empty set
+    assert(s1.size() == 0);             //test size
+    assert(s1.empty());                 //test empty
+    assert(!s1.erase("roti"));          //nothing to remove 
+
+    //functions of one set
+    assert(s1.insert("roti"));          //test insert
+    assert(s1.insert("pita"));          //test insert again
+    assert(s1.size() == 2);             //test size
+    assert(!s1.empty());                //test empty
+    assert(s1.contains("pita"));        //test contains on a present value
+    assert(!s1.contains("spaghetti"));   //test contains on a non-present value
+    assert(s1.erase("roti"));          //test erase on present value
+    assert(!s1.erase("spaghetti"));          //test erase on present value
+    assert(s1.insert("shells"));          //insert another element for later tests
+    
+
+    //copy constructor
+    Set s2(s1);
+
+    //assignment operator
+    Set s3;
+    assert(s3 = s2 == &s3);
+
+
+
+
 }
 
 int main()
