3419c3f19db9cd15700b6fe9106f57ce3e4e040c
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2019-02-09 21:27:27 -0800

    Added HW3 .cpp files; completed implementations and ready to move to final testing and bug fixing

---
+++ HW3/maze.cpp
@@ -0,0 +1,37 @@
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec)
+{
+    if (sr < 0  ||  sr >= nRows  ||  sc < 0  || sc >= nCols  ||
+        er < 0  ||  er >= nRows  ||  ec < 0  || ec >= nCols  ||
+        maze[sr][sc] != '.'  ||  maze[er][ec] != '.')
+       return false;
+
+    if (sr == er  &&  sc == ec)
+        return true;
+
+    //update maze to indicate point has now been found
+    maze[sr][sc] = 'X';
+
+    //check each direction
+    if (maze[sr-1][sc] == ' ')
+    {
+        if(pathExists(maze, nRows, nCols, sr-1, sc, er, ec))
+            return true;
+    }
+    if (maze[sr+1][sc] == ' ')
+    {
+        if(pathExists(maze, nRows, nCols, sr+1, sc, er, ec))
+            return true;
+    }
+    if (maze[sr][sc-1] == ' ')
+    {
+        if(pathExists(maze, nRows, nCols, sr, sc-1, er, ec))
+            return true;
+    }
+    if (maze[sr][sc+1] == ' ')
+    {
+        if(pathExists(maze, nRows, nCols, sr, sc+1, er, ec))
+            return true;
+    }
+
+    return false;
+}
